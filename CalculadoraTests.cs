44dea20d95dd3bd4eab65a332b6c84869fe98bce
Bowie <mariacarolina.mf@gmail.com>
2024-01-10 16:45:17 -0300

    commit inicial

---
+++ CalculadoraTests.cs
@@ -0,0 +1,58 @@
+using Calculadora.Services;
+
+namespace CalculadoraTestes;
+
+public class CalculadoraTests
+{
+    private CalculadoraImplementacao _calc;
+
+    public CalculadoraTests()
+    {
+        _calc = new CalculadoraImplementacao();
+    }
+
+    [Fact]
+    public void DeveSomar5Com10ERetornar15()
+    {
+        //Arrange
+        int num1 = 5;
+        int num2 = 10;
+
+        //Act
+        int resultado = _calc.Somar(num1, num2);
+        
+        //Assert
+        Assert.Equal(15, resultado);
+
+    }
+
+    [Fact]
+    public void DeveVerificarSe4EhParERetornarVerdadeiro()
+    {
+        //Arrange
+        int numero = 4;
+
+        //Act
+        bool resultado = _calc.EhPar(numero);
+
+        //Assert
+        Assert.True(resultado);
+
+
+    }
+
+    [Theory]
+    [InlineData(2)]
+    [InlineData(4)]
+    [InlineData(6)]
+    [InlineData(8)]
+    [InlineData(10)]
+    public void DeveVerificarSeOsNumerosSaoParesERetornarVerdadeiro(int numero)
+    {
+        //Act
+        bool resultado = _calc.EhPar(numero);
+
+        //Assert
+        Assert.True(resultado);
+    }
+}
